4883b9f31518b3e815064e2545e53630321e62cf
Alex Mant <al.mantione@gmail.com>
2022-04-19 13:31:04 +0200

    ajout accept input HTML pour préfiltrer les fichiers

--- Billed-app-FR-Front-main/src/__tests__/Bills.js
+++ Billed-app-FR-Front-main/src/__tests__/Bills.js
@@ -31,7 +31,7 @@ describe("Given I am connected as an employee", () => {
     })
     test("Then bills should be ordered from earliest to latest", () => {
       // triage des factures par date avant l'injection HTML
-      bills.sort((a,b) => (a.date < b.date) ? 1 : -1)
+      bills.sort((a , b) => (a.date < b.date) ? 1 : -1)
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
